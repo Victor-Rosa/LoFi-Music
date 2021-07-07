a069b76c21ef788d80e5598291630e88a73b4d23
Victor Rosa <victorsouzarosa100@gmail.com>
2021-07-07 16:22:07 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let music = document.getElementById("audio")
+let img = document.getElementsByClassName("img_container")[0]
+console.log(img)
+
+function musicPlay(){
+    music.play()
+    img.style.animation = ''
+    img.style.animation = "spin 4s linear infinite"
+    
+}
+
+function musicPause(){
+    music.pause()
+    img.style.animation = ''
+
+}
+
+function musicBack(){
+    music.currentTime -= 15
+}
+
+function musicAvanced(){
+    music.currentTime += 15
+}
